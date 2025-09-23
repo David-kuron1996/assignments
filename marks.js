6440232ad265b030c5ca8dadbdd6c148e72a081b
David <davidkuron1996@gmail.com>
2025-09-23 13:16:49 +0300

    add this file

--- marks.js
+++ marks.js
@@ -1,22 +1,22 @@
-let Marks = (prompt("Enter your marks: "));
+let Marks = (prompt("Enter your marks: ")); // it is the input where user to enter their marks
 
 
 
-getGrade(Marks);
+getGrade(Marks); //this to call the grade function
 
-function getGrade(Marks) {
-    if (Marks > 79 && Marks <= 100) {
-        return grade="A";
-    } else if (Marks >= 60 && Marks <= 79) {
+function getGrade(Marks) {        
+    if (Marks > 79 && Marks <= 100) { //to check if the marks is from 80 to 100(grade=A)
+    return grade="A";
+    } else if (Marks >= 60 && Marks <= 79) { //to check if the marks is from 60 to 79(grade=B)
         return grade="B";
-    } else if (Marks >= 49 && Marks <= 59) {
+    } else if (Marks >= 49 && Marks <= 59) { //to check if the marks is from 49 to 59(grade=C)
         return grade="C";
-    } else if (Marks >= 40 && Marks <= 49) {
+    } else if (Marks >= 40 && Marks <= 49) { //to check if the marks is from 40 to 49(grade=D)
         return grade="D";
-    } else if (Marks >= 0 && Marks <= 39) {
+    } else if (Marks >= 0 && Marks <= 39) { //to check if the marks is from 0 to 39 (grade=E)
         return grade="E";
     } else {
-        return "Invalid marks";
+        return "Invalid marks"; //to return invalid marks if the marks exceed or more than 100
     }
 }
 
--- speed.js
+++ speed.js
@@ -1,16 +1,16 @@
-let speed = (prompt("Enter your speed: "));
+let speed = (prompt("Enter your speed: ")); // it is the input for the speed where the user will enter
 
-checkSpeed(speed);
+checkSpeed(speed); // this method is use to call the checkspeed function 
 
 function checkSpeed(speed) {
-    if (speed <= 70) {
+    if (speed <= 70) { // to check if the speed is less than or equal to 70
         return "Ok";
     } else {
-        let demeritPoints = Math.floor((speed - 70) / 5);
-        if (demeritPoints >= 12) {
+        let demeritPoints = Math.floor((speed - 70) / 5); // this method is to round a number down to the nearest whole integer
+        if (demeritPoints >= 12) { // to check if the demeritpoints is greater than or equal to 12 then it return "License suspended"
             return "License suspended";
         } else {
-            return "Demerit points: " + demeritPoints;
+            return "Demerit points: " + demeritPoints; // this just to display points if it is more than 12 points
         }
     }
 }
